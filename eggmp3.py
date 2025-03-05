487ad7a3141112a969da8e6042511b5ead28fdeb
Yves Miguel <7437383+yvesmre@users.noreply.github.com>
2025-03-05 16:21:48 +0800

    Pretty sure it's working, just not here

--- eggmp3.py
+++ eggmp3.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, sys
 from dotenv import load_dotenv
 import requests
 
@@ -12,7 +12,8 @@ def queryYoutube(string):
 
     query = {
     'key': API_Key,
-    'q': string
+    'q': string,
+    'part': "snippet"
     }
 
     response = requests.request(
@@ -21,7 +22,28 @@ def queryYoutube(string):
     params=query
     )
 
+    # print(response.text)
     return json.loads(response.text)
 
 
-print(queryYoutube()["items"])
+def itemize_result(items):
+
+    results = []
+
+    for result in items:
+        if result['id']["kind"] == "youtube#video":
+            results.append(((result["id"]["videoId"]), result['snippet']['title']))
+
+
+    return results
+
+def main():
+    query = sys.argv[1]
+    for item in itemize_result(queryYoutube(query)["items"]):
+        print(item[1] + "\n")
+        print(item[0] + "\n")
+
+    return itemize_result(queryYoutube(query)["items"])
+
+if __name__ == "__main__":
+    main()
